4967a2220fb872110c65e10267fcf300b76d1848
shyssirixius <ivmikvik@gmail.com>
2017-04-21 22:14:01 +0300

    make var of saas

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Lair
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  
   end
 end
