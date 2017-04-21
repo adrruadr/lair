887e94e2e60abbdba90976c986feb61321c314ed
shyssirixius <ivmikvik@gmail.com>
2017-04-21 23:14:25 +0300

    prodconfig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
-  
+  config.assets.initialize_on_precompile = true
+  config.cache_classes = true
   config.assets.compile = true
   config.serve_static_files = true
   config.assets.digest = true
